e2298a6fb9406cc7314d3983037d7d3a4e155c4d
Harshit-3905 <harshitjoshileet@gmail.com>
2023-07-19 22:21:29 +0530

    Postorder Traversal

---
+++ Binary Trees/Postorder_Traversal.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Postorder_Traversal {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(System.in);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right = new Node(3);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        List<Integer> list = new ArrayList<>();
+        // Solution1(root, list);
+        Solution2(root, list);
+        for (int x : list)
+            System.out.print(x + " ");
+        in.close();
+    }
+
+    // Stack
+    static void Solution(Node root, List<Integer> list) {
+        Stack<Node> s = new Stack<>();
+        s.push(root);
+        while (!s.isEmpty()) {
+            Node curr = s.peek();
+            if (curr.left != null) {
+                s.push(curr.left);
+            } else {
+                list.add(curr.val);
+            }
+        }
+    }
+
+    // Recursion
+    static void Solution2(Node root, List<Integer> list) {
+        if (root != null) {
+            Solution2(root.left, list);
+            Solution2(root.right, list);
+            list.add(root.val);
+        }
+    }
+}
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
