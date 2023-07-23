42b3d278e8c64c8cde40dfbc63a70520dd1a1a7c
Harshit-3905 <harshitjoshileet@gmail.com>
2023-07-23 12:29:02 +0530

    Intersection of Two Linked List

---
+++ Linked List/Intersection_of_Two_Linked_List.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Intersection_of_Two_Linked_List {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        in.close();
+    }
+
+    // Time Complexity : O(m+n)
+    // Space Complexity : O(m)
+    static Node Solution1(Node headA, Node headB) {
+        HashSet<Node> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (set.contains(headB))
+                return headB;
+            headB = headB.next;
+        }
+        return null;
+    }
+
+    // Time Complexity : O(m+n)
+    static Node Solution2(Node headA, Node headB) {
+        Node d1 = headA;
+        Node d2 = headB;
+        while (d1 != d2) {
+            d1 = (d1 == null) ? headB : d1.next;
+            d2 = (d2 == null) ? headA : d2.next;
+        }
+        return d1;
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+
+    Node(int x) {
+        val = x;
+        next = null;
+    }
+}
