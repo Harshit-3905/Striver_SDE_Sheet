4aad747afac834aa3618a125e6b2aeb072c6df4a
Harshit-3905 <harshitjoshileet@gmail.com>
2023-07-24 11:11:30 +0530

    Clone Graph

---
+++ Graph/Clone_Graph.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Clone_Graph {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(System.in);
+        in.close();
+    }
+
+    public Node cloneGraph(Node node) {
+        if (node == null)
+            return null;
+        HashMap<Node, Node> map = new HashMap<>();
+        return Sol(node, map);
+    }
+
+    Node Sol(Node node, HashMap<Node, Node> map) {
+        Node ans = new Node(node.val);
+        map.put(node, ans);
+        for (Node x : node.neighbors) {
+            if (map.containsKey(x)) {
+                ans.neighbors.add(map.get(x));
+                continue;
+            }
+            Sol(x, map);
+            ans.neighbors.add(map.get(x));
+        }
+        return ans;
+    }
+}
+
+class Node {
+    int val;
+    List<Node> neighbors;
+
+    Node(int val) {
+        this.val = val;
+        neighbors = new ArrayList<>();
+    }
+}
