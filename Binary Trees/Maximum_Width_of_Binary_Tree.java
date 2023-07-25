fa6b6e83c5548b2cf5ce11f8bb4d8c5cbb8186cc
Harshit-3905 <harshitjoshileet@gmail.com>
2023-07-25 08:11:04 +0530

    Maximim Width of Binary Tree

--- Binary Trees/Maximum_Width_of_Binary_Tree.java
+++ Binary Trees/Maximum_Width_of_Binary_Tree.java
@@ -1,7 +1,5 @@
 import java.util.*;
 
-import javax.swing.tree.TreeNode;
-
 class Maximum_Width_of_Binary_Tree {
     public static void main(String[] args) throws java.lang.Exception {
         Scanner in = new Scanner(System.in);
@@ -19,23 +17,23 @@ class Maximum_Width_of_Binary_Tree {
     // Time Complexity :O(n)
     // Space Complexity :O(n)
     static int Solution(Node root) {
-        Queue<Pair<Node, Integer>> q = new LinkedList<>();
-        q.add(new Pair<>(root, 1));
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root, 1));
         int max = 0;
         while (!q.isEmpty()) {
             int x = q.size();
             int a = 0, b = 0;
             for (int i = 0; i < x; i++) {
-                Pair<Node, Integer> p = q.poll();
-                Node curr = p.getKey();
+                Pair p = q.poll();
+                Node curr = p.x;
                 if (i == 0)
-                    a = p.getValue();
+                    a = p.y;
                 if (i == x - 1)
-                    b = p.getValue();
+                    b = p.y;
                 if (curr.left != null)
-                    q.add(new Pair<>(curr.left, 2 * p.getValue()));
+                    q.add(new Pair(curr.left, 2 * p.y));
                 if (curr.right != null)
-                    q.add(new Pair<>(curr.right, 2 * p.getValue() + 1));
+                    q.add(new Pair(curr.right, 2 * p.y + 1));
 
             }
             max = Math.max(max, b - a + 1);
@@ -53,5 +51,15 @@ class Node {
         val = x;
         left = null;
         right = null;
+    }
+}
+
+class Pair {
+    Node x;
+    int y;
+
+    Pair(Node x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
