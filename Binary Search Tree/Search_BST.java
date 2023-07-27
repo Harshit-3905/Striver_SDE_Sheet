b75ee2c8c646a01b6e826d2f3858bc59c3c2c0ba
Harshit-3905 <harshitjoshileet@gmail.com>
2023-07-27 09:53:35 +0530

    Search BST

---
+++ Binary Search Tree/Search_BST.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Search_BST {
+
+    static Node searchBST(Node root, int val) {
+        if (root == null || root.val == val)
+            return root;
+        if (root.val < val)
+            return searchBST(root.right, val);
+        else
+            return searchBST(root.left, val);
+    }
+}
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
