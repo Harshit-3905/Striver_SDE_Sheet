eb8b06613bd983f4f37e17f2ec7dfeba7a6037a4
Harshit-3905 <harshitjoshileet@gmail.com>
2024-02-21 13:54:05 +0530

    Top View of Binary Tree

---
+++ Binary Trees/Top_View_of_Binary_Tree.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+
+    static ArrayList<Integer> topView(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Map<Integer, Integer> mpp = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<Pair>();
+
+        q.add(new Pair(root, 0));
+
+        while (!q.isEmpty()) {
+            Pair it = q.remove();
+            int hd = it.hd;
+            Node temp = it.node;
+
+            if (mpp.get(hd) == null) {
+                mpp.put(hd, temp.data);
+            }
+            if (temp.left != null) {
+                q.add(new Pair(temp.left, hd - 1));
+            }
+            if (temp.right != null) {
+                q.add(new Pair(temp.right, hd + 1));
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : mpp.entrySet()) {
+            ans.add(entry.getValue());
+        }
+        return ans;
+
+    }
+}
+
+class Pair {
+    int hd;
+    Node node;
+
+    Pair(Node node, int hd) {
+        this.hd = hd;
+        this.node = node;
+    }
+}
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int key) {
+        data = key;
+        left = right = null;
+    }
+}
