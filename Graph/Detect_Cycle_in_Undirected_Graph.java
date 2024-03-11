194cfaab4fe5694076a4087b631214ae197e0a76
Harshit-3905 <harshitjoshileet@gmail.com>
2024-03-11 12:56:55 +0530

    Detect Cycle in Undirected Graph

--- Graph/Detect_Cycle_in_Undirected_Graph.java
+++ Graph/Detect_Cycle_in_Undirected_Graph.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Detect_Cycle_in_Undirected_Graph {
+class Detect_Cycle_in_Undirected_Graph_Using_DFS {
 
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         boolean vis[] = new boolean[V];
@@ -24,5 +24,41 @@ class Detect_Cycle_in_Undirected_Graph {
                 return true;
         }
         return ans;
+    }
+}
+
+class Detect_Cycle_in_Undirected_Graph_Using_BFS {
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean vis[] = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            if (vis[i])
+                continue;
+            Queue<Pair> q = new LinkedList<>();
+            q.add(new Pair(i, -1));
+            vis[i] = true;
+            while (!q.isEmpty()) {
+                int size = q.size();
+                while (size-- > 0) {
+                    Pair p = q.poll();
+                    for (int x : adj.get(p.vertex)) {
+                        if (!vis[x]) {
+                            vis[x] = true;
+                            q.add(new Pair(x, p.vertex));
+                        } else if (p.parent != x)
+                            return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+class Pair {
+    int vertex, parent;
+
+    Pair(int a, int b) {
+        vertex = a;
+        parent = b;
     }
 }
