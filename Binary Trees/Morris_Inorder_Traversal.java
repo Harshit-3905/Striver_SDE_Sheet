d9af9b9715cb7a0d35f64e371d041e4a9ab4f3c2
Harshit-3905 <harshitjoshileet@gmail.com>
2024-03-15 15:09:51 +0530

    Morris Inorder Traversal

---
+++ Binary Trees/Morris_Inorder_Traversal.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Morris_Inorder_Traversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> inorder = new ArrayList<Integer>();
+
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left == null) {
+                inorder.add(cur.val);
+                cur = cur.right;
+            } else {
+                TreeNode prev = cur.left;
+                while (prev.right != null && prev.right != cur) {
+                    prev = prev.right;
+                }
+
+                if (prev.right == null) {
+                    prev.right = cur;
+                    cur = cur.left;
+                } else {
+                    prev.right = null;
+                    inorder.add(cur.val);
+                    cur = cur.right;
+                }
+            }
+        }
+        return inorder;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
