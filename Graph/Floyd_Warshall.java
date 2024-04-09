59e65c0724f02942219904224ba4feb6ec34fc50
Harshit-3905 <harshitjoshileet@gmail.com>
2024-04-09 12:51:45 +0530

    Floyd Warshall Algorithm

---
+++ Graph/Floyd_Warshall.java
@@ -0,0 +1,24 @@
+class Floyd_Warshall {
+    public void shortest_distance(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == -1)
+                    matrix[i][j] = 999999;
+            }
+        }
+        for (int via = 0; via < n; via++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][via] + matrix[via][j]);
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 999999)
+                    matrix[i][j] = -1;
+            }
+        }
+    }
+}
