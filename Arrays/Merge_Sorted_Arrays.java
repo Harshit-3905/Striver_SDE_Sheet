8c5b1d08aba99978b761d9973b55d24d501039db
Harshit-3905 <harshitjoshileet@gmail.com>
2024-04-27 12:41:41 +0530

    Merge Sorted Arrays

--- Arrays/Merge_Sorted_Arrays.java
+++ Arrays/Merge_Sorted_Arrays.java
@@ -21,9 +21,8 @@ class Merge_Sorted_Arrays {
     // Time Complexity : O(m+n)
     // Space Complexity : O(m+n)
     static void mergeSortedArrays1(int a[], int b[], int m, int n) {
-        int c[] = new int[m];
+        int c[] = new int[m + n];
         int i = 0, j = 0;
-        m -= n;
         int x = 0;
         while (i < m && j < n) {
             if (a[i] < b[j])
@@ -42,6 +41,33 @@ class Merge_Sorted_Arrays {
     // Time Complexity : O(m+n)
     // Space Complexity : O(1)
     static void mergeSortedArrays2(int a[], int b[], int m, int n) {
+        int len = n + m;
+        int gap = (len / 2) + (len % 2);
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+            while (right < len) {
+                if (left < n && right >= n) {
+                    swapIfGreater(a, b, left, right - n);
+                } else if (left >= n) {
+                    swapIfGreater(a, b, left - n, right - n);
+                } else {
+                    swapIfGreater(a, b, left, right);
+                }
+                left++;
+                right++;
+            }
+            if (gap == 1)
+                break;
+            gap = (gap / 2) + (gap % 2);
+        }
+    }
 
+    public static void swapIfGreater(int[] arr1, int[] arr2, int ind1, int ind2) {
+        if (arr1[ind1] > arr2[ind2]) {
+            int temp = arr1[ind1];
+            arr1[ind1] = arr2[ind2];
+            arr2[ind2] = temp;
+        }
     }
 }
